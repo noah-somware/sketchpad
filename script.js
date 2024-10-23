951020a2d7e603d0ba2ec04f0dc9ef22311d6c30
Noah Church mobile <noah@somewhereapp.com>
2024-10-23 16:04:12 -0700

    Add support for various modes
    basic, rainbow, and shader modes are now available and working. User
    can randomize color. Color picker button is added but not yet functional.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let gridCount = 16;
 
+// possible modes: "basic", "shader", "rainbow"
+let mode = 'basic';
+let red = 0;
+let green = 0;
+let blue = 0;
+
+let color = `rgb(${red}, ${green}, ${blue})`;
+
+function randomizeColor() {
+    red = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+
+    color = `rgb(${red}, ${green}, ${blue})`;
+    console.log(`new color elements: rgb(${red}, ${green}, ${blue})`);
+    console.log('new color variable: ', color);
+}
+
 const gridCountNumber = document.querySelectorAll('.gridCountNumber');
 
 const getNumber = () => {
@@ -22,13 +40,17 @@ function populateGrid() {
         sketchContainer.appendChild(yDiv);
         for (let x = 0; x < gridCount; x++) {
             let xDiv = document.createElement('div');
-            xDiv.classList.add('xDiv', 'inactiveNode');
+            xDiv.classList.add(
+                'xDiv'
+                // , 'inactiveNode'
+            );
+            xDiv.style.backgroundColor = 'transparent';
             yDiv.appendChild(xDiv);
         }
     }
 
     gridCountNumber.forEach((element) => {
-        console.log("Setting gridCountNumbers");
+        console.log('Setting gridCountNumbers');
         element.textContent = gridCount;
     });
 }
@@ -38,7 +60,24 @@ let isMouseDown = false;
 function activateNode(e) {
     if (isMouseDown && e.target.classList.contains('xDiv')) {
         console.log('activating node.');
-        e.target.classList.add('activeNode');
+        // e.target.classList.add('activeNode');
+        switch (mode) {
+            case 'shader':
+                e.target.style.backgroundColor = 'black';
+                console.log('about to populate currentOpacity to: ', e.target.style.opacity);
+                const currentOpacity = parseFloat(e.target.style.opacity);
+                console.log('currentOpacity: ', currentOpacity);
+                currentOpacity > 0.9 ? (e.target.style.opacity = 1) : (e.target.style.opacity = currentOpacity + 0.1);
+                console.log('new opacity: ', e.target.style.opacity);
+                break;
+            case 'rainbow':
+                randomizeColor();
+                e.target.style.backgroundColor = color;
+                break;
+            default:
+                e.target.style.backgroundColor = color;
+                break;
+        }
     }
 }
 
@@ -59,12 +98,13 @@ function resetNodes() {
     console.log('resetting nodes');
     const allX = document.querySelectorAll('.xDiv');
     allX.forEach((element) => {
-        element.classList.remove('activeNode');
+        // element.classList.remove('activeNode');
+        element.style.backgroundColor = 'transparent';
     });
+    color = `rgb(${red}, ${green}, ${blue})`;
 }
 
 const buttonsContainer = document.querySelector('#buttons');
-
 buttonsContainer.addEventListener('click', (e) => {
     console.log('target id: ', e.target.id);
     switch (e.target.id) {
@@ -79,5 +119,40 @@ buttonsContainer.addEventListener('click', (e) => {
             break;
         default:
             console.error('Error clicking button.');
+    }
+});
+
+const modeButtonsContainer = document.querySelector('#modeButtons');
+modeButtonsContainer.addEventListener('click', (e) => {
+    console.log('target id: ', e.target.id);
+    const allX = document.querySelectorAll('.xDiv');
+    switch (e.target.id) {
+        case 'colorPicker':
+            allX.forEach((element) => {
+                element.style.opacity = 1;
+            });
+            mode = 'basic';
+            break;
+        case 'randomColor':
+            allX.forEach((element) => {
+                element.style.opacity = 1;
+            });
+            mode = 'basic';
+            randomizeColor();
+            break;
+        case 'rainbow':
+            allX.forEach((element) => {
+                element.style.opacity = 1;
+            });
+            mode = 'rainbow';
+            break;
+        case 'shader':
+            allX.forEach((element) => {
+                element.style.opacity = 0;
+            });
+            mode = 'shader';
+            break;
+        default:
+            console.error('Error in clicking modeButtons');
     }
 });
